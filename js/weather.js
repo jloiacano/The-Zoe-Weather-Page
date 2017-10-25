9fb23f0fc00e8ca7af293a6153dd7a22f136c55d
jloiacano <j.loiacano55@gmail.com>
2017-10-25 13:06:46 -0500

    beat it with a bootstrap, added button styling, location and settings viewing functionality etc...

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,9 @@
+/*global $, navigator, window*/
+
 (function () {
     "use strict";
 
-    var test,
-        WEATHER_JSON,
+    var WEATHER_JSON,
         colorIridescentFuchsia = "rgba(250, 5, 108, 1.0)",
         colorIridescentTurquoise = "rgba(5, 250, 147, 1.0)",
         colorIridescentCobalt = "rgba(26, 5, 250, 1.0)",
@@ -10,12 +11,8 @@
         colorPastelTurquoise = "rgba(180, 250, 221, 1.0)",
         colorPastelCobalt = "rgba(186, 180, 250, 1.0)",
         colorDarkFuchsia = "rgba(122, 2, 53, 1.0)",
-        colorDarkTurquoise =  "rgba(2, 122, 71, 1.0)",
+        colorDarkTurquoise = "rgba(2, 122, 71, 1.0)",
         colorDarkCobalt = "rgba(11, 2, 122, 1.0)";
-
-
-    /* jslint browser */
-    /* global window, document, navigator */
 
     function codeToImage(code) {
         switch (code) {
@@ -79,41 +76,34 @@
         }
     }
 
-    function changeTheColors() {
-        window.console.log("color changer clicked and changed to: ");
-        if ($("input:radio[name='colors']:checked").val() === "iridescent") {
-            window.console.log("iridescent");
-            $("body").css("background-color", colorIridescentFuchsia);
-            $("body").css("color", colorIridescentCobalt);
-            $(".location").css("background-color", colorIridescentTurquoise);
-            $(".settings").css("background-color", colorIridescentTurquoise);
-            $("#otherData").css("background-color", colorIridescentTurquoise);
-            $(".forcastDayDiv").css("border", "1px solid " + colorIridescentTurquoise);
-            $("img").css("filter", "brightness(100%)");
-            $("img").css("-webkit-filter", "brightness(100%)");
+    function showLocationInputs() {
+        if ($("#locationInputsWrapper").css("display") === "none") {
+            $('#locationInputsWrapper').show();
+            $('#locationAndSettingsImagesWrapper').hide();
 
-        } else if ($("input:radio[name='colors']:checked").val() === "pastel") {
-            window.console.log("pastel");
-            $("body").css("background-color", colorPastelFuchsia);
-            $("body").css("color", colorPastelCobalt);
-            $(".location").css("background-color", colorPastelTurquoise);
-            $(".settings").css("background-color", colorPastelTurquoise);
-            $("#otherData").css("background-color", colorPastelTurquoise);
-            $(".forcastDayDiv").css("border", "1px solid " + colorPastelTurquoise);
-            $("img").css("filter", "brightness(100%)");
-            $("img").css("-webkit-filter", "brightness(100%)");
-
-        } else if ($("input:radio[name='colors']:checked").val() === "dark") {
-            window.console.log("dark");
-            $("body").css("background-color", colorDarkFuchsia);
-            $("body").css("color", colorDarkCobalt);
-            $(".location").css("background-color", colorDarkTurquoise);
-            $(".settings").css("background-color", colorDarkTurquoise);
-            $("#otherData").css("background-color", colorDarkTurquoise);
-            $(".forcastDayDiv").css("border", "1px solid " + colorDarkTurquoise);
-            $("img").css("filter", "brightness(50%)");
-            $("img").css("-webkit-filter", "brightness(50%)");
+        } else {
+            $('#locationInputsWrapper').hide();
+            $('#locationAndSettingsImagesWrapper').show();
         }
+    }
+
+    function showSettingsInputs() {
+        if ($("#settingsInputsWrapper").css("display") === "none") {
+            $('#settingsInputsWrapper').show();
+            $('#locationAndSettingsImagesWrapper').hide();
+        } else {
+            $('#settingsInputsWrapper').hide();
+            $('#locationAndSettingsImagesWrapper').show();
+        }
+    }
+
+    function forceLocationEntry() {
+        window.console.log("forceLocationEntry invoked");
+        showLocationInputs();
+        $("#main").hide();
+        $("#city").css("border", "2px solid rgba(5, 250, 24, 1.0)");
+        $("#state").css("border", "2px solid rgba(5, 250, 24, 1.0)");
+
     }
 
     function doSomethingWithTheWeather() {
@@ -122,16 +112,17 @@
             respoonseCity = weather.results.channel.location.city,
             responseState = weather.results.channel.location.region,
             currentTemp = weather.results.channel.item.condition.temp,
-            forcast = weather.results.channel.item.forecast,
-            src = "",
-            day = "",
-            i;
+            forecast = weather.results.channel.item.forecast,
+            i,
+            template = $(".forecastDayDiv"),
+            clone;
 
         $("#time").text("The weather for " + respoonseCity + ", " + responseState + " at " + time);
-        $("#todaysImage").attr('src', codeToImage(forcast[0].code));
+        $("#todaysImage").attr('src', codeToImage(forecast[0].code));
         $("#currentTemp").text(currentTemp);
-        $("#todaysHigh").text(forcast[0].high);
-        $("#todaysLow").text(forcast[0].low);
+        $("#todaysHigh").text(forecast[0].high);
+        $("#todaysLow").text(forecast[0].low);
+        $(".weatherLocation").text(respoonseCity + ", " + responseState);
         $("#windChill").text(weather.results.channel.wind.chill);
         $("#windDirection").text(weather.results.channel.wind.direction);
         $("#windSpeed").text(weather.results.channel.wind.speed);
@@ -143,14 +134,19 @@
         $("#astronomySunset").text(weather.results.channel.astronomy.sunset);
 
 
-        $("#forecast").html("");
+        $("#forecast").children().empty();
+
         // set the forecast data and append it to the forecast div
-        for (i = 1; i <= forcast.length - 1; i += 1) {
-            src = codeToImage(forcast[i].code);
-
-            day = '<div class="forcastDayDiv"><img class="forecastImage weatherImage" src="' + src + '" alt="weatherType" /><span class="forecastDayAndDate"><span class="forecastDay">' + forcast[i].day + '</span><span class="forecastDate">' + forcast[i].date + '</span></span><span class="wholeForecastHigh">HIGH: <span class="forecastHigh">' + forcast[i].high + '</span><span>°</span></span><span class="wholeForecastLow">LOW: <span class="forecastLow">' + forcast[i].low + '</span><span>°</span></span><span class="forecastText">' + forcast[i].text + '</span></div>';
-
-            $("#forecast").append(day);
+        for (i = 1; i <= forecast.length - 1; i += 1) {
+            clone = template.clone();
+            $("#forecast").append(clone);
+            clone.find(".forecastImage").attr('src', codeToImage(forecast[i].code));
+            clone.find(".forecastDay").append(forecast[i].day);
+            clone.find(".forecastDate").append(forecast[i].date);
+            clone.find(".forecastHigh").append("HIGH: " + forecast[i].high + "°");
+            clone.find(".forecastLow").append("LOW: " + forecast[i].low + "°");
+            clone.find(".forecastText").append(forecast[i].text);
+            clone.show();
         }
     }
 
@@ -166,31 +162,13 @@
 
         if ($("input:radio[name='scale']:checked").val() === "celsius") {
             WEATHER_JSON.results.channel.item.condition.temp = toC(WEATHER_JSON.results.channel.item.condition.temp);
+            WEATHER_JSON.results.channel.wind.chill = toC(WEATHER_JSON.results.channel.wind.chill);
             for (i = 0; i < WEATHER_JSON.results.channel.item.forecast.length; i += 1) {
                 WEATHER_JSON.results.channel.item.forecast[i].high = toC(WEATHER_JSON.results.channel.item.forecast[i].high);
                 WEATHER_JSON.results.channel.item.forecast[i].low = toC(WEATHER_JSON.results.channel.item.forecast[i].low);
             }
         }
         doSomethingWithTheWeather();
-    }
-
-    function loadFromCityAndState() {
-
-        //// temp temp temp temp ////
-        var city = $("#city").val(),
-            state = $("#state").val();
-        //// temp temp temp temp ////
-
-        $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + ',' + state + '")&format=json&env=store://datatables.org/alltableswithkeys', setWeather);
-
-    }
-
-    function forceLocationEntry() {
-        // if location entry is already set, loadFromCityAndState();
-        //openLocationMenu
-        //highlight city, then state after city is entered
-        // loadFromCityAndState();
-
     }
 
     function getSomeWeather() {
@@ -209,19 +187,166 @@
 
         function error() {
             window.console.log("ERROR RETRIEVING YAHOO WEATHER JSON RESPONSE");
+            forceLocationEntry();
         }
 
         navigator.geolocation.getCurrentPosition(success, error);
     }
 
-    function testFunction() {
-        window.alert("testing");
+    function changeTheColors() {
+        window.console.log("color changer clicked and changed to: ");
+        if ($("input:radio[name='colors']:checked").val() === "iridescent") {
+            window.console.log("iridescent");
+            $("body").css("background-color", colorIridescentFuchsia);
+            $("body").css("color", colorIridescentCobalt);
+            $(".btn-2g").css("background-color", colorIridescentTurquoise);
+            $(".btn-2g").css("color", colorIridescentFuchsia);
+            $(".btn-2g").hover(function () {
+                $(this).css("background-color", colorIridescentFuchsia);
+            }, function () {
+                $(this).css("background-color", colorIridescentTurquoise);
+            });
+            $(".btn-2g").hover(function () {
+                $(this).css("color", colorIridescentTurquoise);
+            }, function () {
+                $(this).css("color", colorIridescentFuchsia);
+            });
+            $("#otherData").css("background-color", colorIridescentTurquoise);
+            $(".relativeWrapper").css("border", "1px solid " + colorIridescentTurquoise);
+            $("img").css("filter", "brightness(100%)");
+            $("img").css("-webkit-filter", "brightness(100%)");
+
+        } else if ($("input:radio[name='colors']:checked").val() === "pastel") {
+            window.console.log("pastel");
+            $("body").css("background-color", colorPastelFuchsia);
+            $("body").css("color", colorPastelCobalt);
+            $(".btn-2g").css("background-color", colorPastelTurquoise);
+            $(".btn-2g").css("color", colorPastelFuchsia);
+            $(".btn-2g").hover(function () {
+                $(this).css("background-color", colorPastelFuchsia);
+            }, function () {
+                $(this).css("background-color", colorPastelTurquoise);
+            });
+            $(".btn-2g").hover(function () {
+                $(this).css("color", colorPastelTurquoise);
+            }, function () {
+                $(this).css("color", colorPastelFuchsia);
+            });
+            $("#otherData").css("background-color", colorPastelTurquoise);
+            $(".relativeWrapper").css("border", "1px solid " + colorPastelTurquoise);
+            $("img").css("filter", "brightness(100%)");
+            $("img").css("-webkit-filter", "brightness(100%)");
+
+        } else if ($("input:radio[name='colors']:checked").val() === "dark") {
+            window.console.log("dark");
+            $("body").css("background-color", colorDarkFuchsia);
+            $("body").css("color", colorDarkCobalt);
+            $(".btn-2g").css("background-color", colorDarkTurquoise);
+            $(".btn-2g").css("color", colorDarkFuchsia);
+            $(".btn-2g").hover(function () {
+                $(this).css("background-color", colorDarkFuchsia);
+            }, function () {
+                $(this).css("background-color", colorDarkTurquoise);
+            });
+            $(".btn-2g").hover(function () {
+                $(this).css("color", colorDarkTurquoise);
+            }, function () {
+                $(this).css("color", colorDarkFuchsia);
+            });
+            $("#otherData").css("background-color", colorDarkTurquoise);
+            $(".relativeWrapper").css("border", "1px solid " + colorDarkTurquoise);
+            $("img").css("filter", "brightness(50%)");
+            $("img").css("-webkit-filter", "brightness(50%)");
+        }
+        showSettingsInputs();
+        getSomeWeather();
     }
 
-    test = getSomeWeather();
+    function loadFromCityAndState() {
+        $("#main").show();
+        var city = $("#city").val(),
+            state = $("#state").val();
+
+        $("#city").css("border", "initial");
+        $("#state").css("border", "initial");
+
+        showLocationInputs();
+        $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + ',' + state + '")&format=json&env=store://datatables.org/alltableswithkeys', setWeather);
+
+    }
+
+    function showAbout() {
+        window.console.log("ABOUT");
+    }
+
+
+
+    getSomeWeather();
 
     $("#cityAndState").click(loadFromCityAndState);
-    $("#changeScale").click(getSomeWeather);
-    $("#changeColors").click(changeTheColors);
+    $("#updateSettings").click(changeTheColors);
+    $("#locationImage").click(showLocationInputs);
+    $("#settingsImage").click(showSettingsInputs);
+    $("#about").click(showAbout);
 
 }());
+
+//        switch (code) {
+//        case "26":
+//        case "27":
+//        case "28":
+//            return "imageAssets/cloudyCroppedTransparent.png";
+//        case "29":
+//        case "30":
+//        case "44":
+//            return "imageAssets/partlyCloudyCroppedTransparent.png";
+//        case "1":
+//        case "3":
+//        case "4":
+//        case "9":
+//        case "11":
+//        case "12":
+//        case "35":
+//        case "37":
+//        case "38":
+//        case "39":
+//        case "40":
+//        case "45":
+//        case "46":
+//        case "47":
+//            return "imageAssets/rainCroppedTransparent.png";
+//        case "5":
+//        case "6":
+//        case "7":
+//        case "8":
+//        case "10":
+//        case "13":
+//        case "14":
+//        case "15":
+//        case "16":
+//        case "17":
+//        case "18":
+//        case "41":
+//        case "42":
+//        case "43":
+//            return "imageAssets/snowCroppedTransparent.png";
+//        case "31":
+//        case "32":
+//        case "33":
+//        case "34":
+//        case "36":
+//            return "imageAssets/sunnyCroppedTransparent.png";
+//        case "0":
+//        case "2":
+//        case "19":
+//        case "20":
+//        case "21":
+//        case "22":
+//        case "23":
+//        case "24":
+//        case "25":
+//            return "imageAssets/windyCroppedTransparent.png";
+//        default:
+//            window.console.log("ERROR WITH codeToImage()");
+//            break;
+//    }
